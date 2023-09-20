7ffa3be7e70893862acad728f79c709b219b2fa9
danilojezernik <dani.jezernik@gmail.com>
2023-09-20 22:00:58 +0200

    USCOM

--- src/router/email.py
+++ src/router/email.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, HTTPException
-
+from fastapi import APIRouter, HTTPException, BackgroundTasks
 from src.domain.mail import Email
 from src.services import emails, db
 from src.template import email
@@ -7,22 +6,25 @@ from src.template import email
 router = APIRouter()
 
 
-# USER SENDING EMAIL TO AUTHOR
 @router.post("/send-email")
-async def sign_in(emailing: Email):
+async def send_email(emailing: Email, background_tasks: BackgroundTasks):
+    # Check for white spaces in name and surname
     if ' ' in emailing.name or len(emailing.name) < 3:
-        return HTTPException(status_code=400, detail="There are white spaces in name")
+        raise HTTPException(status_code=400, detail="There are white spaces in name")
 
     if ' ' in emailing.surname or len(emailing.surname) < 3:
-        return HTTPException(status_code=400, detail="There are white spaces in surname")
+        raise HTTPException(status_code=400, detail="There are white spaces in surname")
 
+    # Check if email exists
     if not emails.exists(emailing.email):
-        return HTTPException(status_code=400, detail="Email not existing")
+        raise HTTPException(status_code=400, detail="Email not existing")
 
     body = email.html(name=emailing.name, surname=emailing.surname, email=emailing.email, content=emailing.content)
 
-    if not emails.send(email_to=emailing.email, subject='USCOM | You got email ♥', body=body):
-        return HTTPException(status_code=500, detail="Email not sent")
+    # Send the email in the background
+    background_tasks.add_task(
+        emails.send, email_to=emailing.email, subject='USCOM | You got email ♥', body=body
+    )
 
     email_data = {
         "name": emailing.name,
@@ -31,6 +33,7 @@ async def sign_in(emailing: Email):
         "content": emailing.content
     }
 
+    # Insert email data into the database
     db.api.email.insert_one(email_data)
 
     return {"message": "Message was sent"}
